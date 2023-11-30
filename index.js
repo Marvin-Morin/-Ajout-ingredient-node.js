28f49bebf54597282c3563f8f3ea81021d200317
Marvin Morin <94224087+Marvin-Morin@users.noreply.github.com>
2023-11-30 08:47:48 +0100

    index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');  // Ajoutez cette ligne
+const path = require('path'); 
 
 const app = express();
 const PORT = 4000;
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));  // Ajoutez cette ligne
+app.set('views', path.join(__dirname, 'views')); 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
